885051313e6cac5b771cfddcc79500b19ba0e260
Christian <christian@serverless.industries>
2023-06-25 17:06:51 +0200

    curl multiarch

--- src/curl/root/src/build.sh
+++ src/curl/root/src/build.sh
@@ -32,23 +32,8 @@ tar xzf curl-${CURL_VERSION}.tar.gz
 
 cd curl-${CURL_VERSION}/
 
-# dependencies to build curl
-apk add build-base clang openssl-dev nghttp2-dev nghttp2-static libssh2-dev libssh2-static
-
-# these are missing on at least armhf
-apk add openssl-libs-static zlib-static || true
-
 # gcc is apparantly incapable of building a static binary, even gcc -static helloworld.c ends up linked to libc, instead of solving, use clang
 export CC=clang
-
-# apply patches if needed
-#patch -p1 < ../static.patch
-#apk add autoconf automake libtool
-#autoreconf -fi
-# end apply patches
-
-# set up any required curl options here
-#LDFLAGS="-static" PKG_CONFIG="pkg-config --static" ./configure --disable-shared --enable-static --disable-libcurl-option --without-brotli --disable-manual --disable-unix-sockets --disable-dict --disable-file --disable-gopher --disable-imap --disable-smtp --disable-rtsp --disable-telnet --disable-tftp --disable-pop3 --without-zlib --disable-threaded-resolver --disable-ipv6 --disable-smb --disable-ntlm-wb --disable-tls-srp --disable-crypto-auth --without-ngtcp2 --without-nghttp2 --disable-ftp --disable-mqtt --disable-alt-svc --without-ssl
 
 LDFLAGS="-static" PKG_CONFIG="pkg-config --static" ./configure --disable-shared --enable-static --disable-ldap --enable-ipv6 --enable-unix-sockets --with-ssl --with-libssh2
 
@@ -65,10 +50,6 @@ ldd src/curl && exit 1 || true
 
 ./src/curl -V
 
-#./src/curl -v http://www.moparisthebest.com/; ./src/curl -v https://www.moparisthebest.com/ip
-
 # we only want to save curl here
-mkdir -p /tmp/release/
-mv src/curl "/tmp/release/curl-$ARCH"
-cd ..
-rm -rf "curl-${CURL_VERSION}/"
+mkdir -p /dist
+mv src/curl "/dist/curl-$ARCH"
